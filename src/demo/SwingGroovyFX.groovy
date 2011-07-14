bc732bd6afbb0ba71bd8f0391890d06803658b99
dean <dean@f908d50e-8e36-0410-ac0a-93754bd5008d>
2011-07-14 16:12:39 +0000

    Updated the SwingGroovyFX demo to make it a simple browser application.

--- src/demo/SwingGroovyFX.groovy
+++ src/demo/SwingGroovyFX.groovy
@@ -8,35 +8,36 @@ package demo
 import groovyx.javafx.SceneGraphBuilder
 import groovyx.javafx.GroovyFX;
 import javafx.embed.swing.JFXPanel
-import javafx.application.Platform;
 import groovy.swing.SwingBuilder
 import java.awt.BorderLayout as BL
-import java.awt.Dimension;
+import java.awt.Dimension
+import javafx.application.Platform;
 
+def DEFAULT_URL = "http://www.yahoo.com"
 def swing = new SwingBuilder();
+def fxPanel = new JFXPanel(preferredSize: new Dimension(800,400));
 
+def setUrl = { url ->
+    Platform.runLater {
+        webEngine.load(url)
+    }
+}
 
-
-
-count = 0
-def fxPanel = new JFXPanel(preferredSize: new Dimension(100,100));
-
-def fr = null;
 swing.edt {
-  fr = frame(title:'Frame', size:[300,300], show: true) {
-    borderLayout()
-    textlabel = label(text:"Click the button!", constraints: BL.NORTH)
-    widget(fxPanel, constraints: BL.CENTER)
-    button(text:'Click Me',
-         actionPerformed: {count++; textlabel.text = "Clicked ${count} time(s)."; println "clicked"},
-         constraints:BL.SOUTH)
-  }
+    frame(title:'Swing Embedded Browser', size:[800,500], show: true) {
+        borderLayout()
+        panel(constraints: BL.NORTH) {
+            textField(id: "urlField", actionPerformed: {setUrl(urlField.text)}, columns: 40)
+            button(text: "Go", actionPerformed: {setUrl(urlField.text)})
+        }
+        widget(fxPanel, constraints: BL.CENTER)
+    }
 }
 
 GroovyFX.start {
      def sg = new SceneGraphBuilder(it);
-     fxPanel.scene = sg.scene(width: 100, height: 100) {
-         fill red
-         rectangle(x: 30, y: 20, width: 10, height: 10, fill: green)
+     fxPanel.scene = sg.scene(width: 800, height: 400) {
+         webEngine = webEngine(location: DEFAULT_URL)
+         webView(engine: webEngine)
      }
 }
