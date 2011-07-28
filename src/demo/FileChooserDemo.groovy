3095ac9ac2498f19e9e57831b1ffdaf4872565fb
jimclarke5 <jimclarke5@f908d50e-8e36-0410-ac0a-93754bd5008d>
2011-07-28 00:10:19 +0000

    Add new features from latest JavaFX release

---
+++ src/demo/FileChooserDemo.groovy
@@ -0,0 +1,52 @@
+/*
+* Copyright 2011 the original author or authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package demo
+
+import groovyx.javafx.GroovyFX;
+import groovyx.javafx.SceneGraphBuilder
+
+GroovyFX.start({ primaryStage -> 
+    def sg = new SceneGraphBuilder(primaryStage);
+    
+    def fileChooser = sg.fileChooser(initialDirectory: ".", title: "FileChooser Demo") {
+        filter("images", extensions: ["jpg", "gif", "bmp"])
+    }
+    def stage = sg.stage(
+        title: "File Chooser Demo",
+        width: 400, height:300,
+        visible: true,
+        resizable: true
+    ) {
+         scene(fill: lightgreen) {
+             stackPane() {
+                 hbox(spacing: 10) {
+                    button("Open file", onAction: { println(fileChooser.showOpenDialog(stage));})
+                    button("Save file", onAction: { println(fileChooser.showSaveDialog(stage));})
+                 }
+             } 
+         }
+    }
+    
+     
+    
+    
+    
+    
+})
+
+
+
---
+++ src/demo/TextFieldDemo.groovy
@@ -0,0 +1,49 @@
+/*
+* Copyright 2011 the original author or authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package demo
+
+import groovyx.javafx.SceneGraphBuilder
+import groovyx.javafx.GroovyFX;
+/**
+ *
+ * @author jimclarke
+ */
+
+GroovyFX.start({
+def sg = new SceneGraphBuilder(it);
+
+sg.stage(
+    title: "TextField Demo",
+    x: 100, y: 100,
+    visible: true,
+    style: "decorated",
+    primary: true
+) {
+
+    scene(fill: hsb(128, 0.5, 0.5, 0.5), width: 400, height:400 ) {
+        borderPane() {
+            text = textField(promptText: 'Type here', prefColumnCount:80, onAction: { println "T: " + text.text;})
+            bottom(align: "center", margin: [10,0]) {
+                button(text: "Print Text", onAction: { println text.text })
+            }
+        }
+    }
+}
+
+});
+
+
