cd9e84c9c754ed7f7ee9d00a280f81bc471bd8bd
dean <dean@f908d50e-8e36-0410-ac0a-93754bd5008d>
2011-08-06 15:16:34 +0000

    Made the rectangle translucent and added a click handler to restart the animation.

--- src/demo/PathTransitionDemo.groovy
+++ src/demo/PathTransitionDemo.groovy
@@ -19,27 +19,32 @@ import groovyx.javafx.GroovyFX
 import groovyx.javafx.SceneGraphBuilder
 import javafx.animation.Transition
 import javafx.animation.PathTransition.OrientationType
+import javafx.scene.paint.Color
 
-GroovyFX.start { primaryStage -> 
+GroovyFX.start { primaryStage ->
     def sg = new SceneGraphBuilder(primaryStage)
     Transition rectTransition = null
 
+    def playTransition = {
+        rectTransition.playFromStart()
+    }
+    
     sg.stage(title: "GroovyFX Path Transition Demo", width: 400, height:300, visible: true, resizable: true) {
          scene(fill: groovyblue) {
              def thePath = path( translateX: 50, translateY: 50, fill: transparent, stroke: green, strokeWidth: 5) {
-                moveTo(x: 0, y: 0)
-                lineTo(x: 100, y: 0)
-                arcTo(x: 200, y: 0, radiusX: 25, radiusY: 20)
-                lineTo(x: 300, y: 0)
-                lineTo(x: 150, y: 100)
-                closePath()
+                 moveTo(x: 0, y: 0)
+                 lineTo(x: 100, y: 0)
+                 arcTo(x: 200, y: 0, radiusX: 25, radiusY: 20)
+                 lineTo(x: 300, y: 0)
+                 lineTo(x: 150, y: 100)
+                 closePath()
              }
-             rectangle(width: 20, height: 50, fill: blue) {
+             rectangle(width: 20, height: 50, fill: Color.web("#00F8"), onMousePressed: {playTransition()}) {
                 rectTransition = pathTransition(20.s, delay: 100.ms, path: thePath, onFinished: {println "done"},
                                                 orientation: OrientationType.ORTHOGONAL_TO_TANGENT)
              }
          }
     }
-    rectTransition.playFromStart()
+    playTransition()
 }
 
