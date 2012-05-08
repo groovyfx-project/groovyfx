d283ffbd512048e000d73d103d4c58ddd66e1934
JimClarke5 <JimClarke5@aol.com>
2012-05-08 10:19:50 -0400

    Fix 2 stage demo to eliminate race condistion and make demo code cleaner

--- groovyfx/src/demo/groovy/TwoStagesDemo.groovy
+++ groovyfx/src/demo/groovy/TwoStagesDemo.groovy
@@ -1,18 +1,12 @@
 import groovyx.javafx.SceneGraphBuilder
 import groovyx.javafx.GroovyFX;
-def sg = new SceneGraphBuilder();
-Thread.startDaemon({ GroovyFX.start{} });
-sg.defer {
-	def stage1 = sg.stage(primary: false, x:100, y: 100, onShown: { println "Stage 1"}) {
-		scene(width: 100, height: 100, fill:groovyblue)
-        }
-	stage1.show();
-}
 
-sg.defer {
-	def stage2 = sg.stage(primary: false, x: 100, y: 400) {
-            onShown { println "Stage 2" }
-            scene(width: 100, height: 100, fill:red)
-        }
-	stage2.show()
+GroovyFX.start{ 
+    stage( x:100, y: 100, onShown: { println "Stage 1"}, visible: true) {
+            scene(width: 100, height: 100, fill:groovyblue)
+    }
+    stage(primary: false, x: 100, y: 400, visible: true) {
+        onShown { println "Stage 2" }
+        scene(width: 100, height: 100, fill:red)
+    }
 }
