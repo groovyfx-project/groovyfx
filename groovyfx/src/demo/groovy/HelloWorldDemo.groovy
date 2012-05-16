61f88002235e8e2bdea466c1ca28207d137ada79
Dean Iverson <dean@pleasingsoftware.com>
2012-05-16 13:29:29 -0600

    Removed IDEA project files in favor of Gradle generation.  Latest changes to the 0.2 docs.

--- groovyfx/src/demo/groovy/HelloWorldDemo.groovy
+++ groovyfx/src/demo/groovy/HelloWorldDemo.groovy
@@ -17,13 +17,13 @@
 import static groovyx.javafx.GroovyFX.start
 
 start {
-    stage(title: "GroovyFX Hello World", visible: true) {
+    stage(title: 'GroovyFX Hello World', visible: true) {
         scene(fill: BLACK, width: 500, height: 250) {
             hbox(padding: 60) {
-                text(text: "Groovy", style: "-fx-font-size: 80pt") {
+                text(text: 'Groovy', font: '80pt sanserif') {
                     fill linearGradient(endX: 0, stops: [PALEGREEN, SEAGREEN])
                 }
-                text(text: "FX", style: "-fx-font-size: 80pt") {
+                text(text: 'FX', font: '80pt sanserif') {
                     fill linearGradient(endX: 0, stops: [CYAN, DODGERBLUE])
                     effect dropShadow(color: DODGERBLUE, radius: 25, spread: 0.25)
                 }
