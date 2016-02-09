5f69e90d3e6f8acb887143ea38f35b2cdd3e6858
aalmiray <aalmiray@gmail.com>
2016-02-09 13:22:57 +0100

    Update license headers

--- src/demo/groovy/FXMLDemoFromSceneBuilderFile.groovy
+++ src/demo/groovy/FXMLDemoFromSceneBuilderFile.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011-2015 the original author or authors.
+ * Copyright 2011-2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 /**
  * Show how to embed an FXML file that was created with SceneBuilder and
  * get access to the components by id.
